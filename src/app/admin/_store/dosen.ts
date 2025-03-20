e68e7b8eb044480fa09dd1808d0292a29eee75fa
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2025-03-20 12:39:57 +0800

    [_store:dosen] Add `update` method

--- src/app/admin/_store/dosen.ts
+++ src/app/admin/_store/dosen.ts
@@ -13,6 +13,7 @@ type DosenStore = {
   setData: (data: DosenItem[]) => void;
   addData: (data: DosenItem) => void;
   deleteData: (id: DosenItem['id']) => void;
+  updateData: (id: DosenItem['id'], nama: DosenItem['nama'], mata_kuliah: DosenItem['mata_kuliah']) => void;
 }
 
 export const useDosenStore = create<DosenStore>((set) => ({
@@ -28,6 +29,16 @@ export const useDosenStore = create<DosenStore>((set) => ({
   }),
   deleteData: (id: DosenItem['id']) => set((state) => {
     const newData = state.data.filter((item) => item.id !== id)
+    return {
+      ...state,
+      data: newData
+    }
+  }),
+  updateData: (id: DosenItem['id'], nama: DosenItem['nama'], mata_kuliah: DosenItem['mata_kuliah']) => set((state) => {
+    const newData = state.data.map((item) => item.id !== id 
+    ? item : {
+      ...item, 'nama': nama, 'mata_kuliah': mata_kuliah
+    })
     return {
       ...state,
       data: newData
