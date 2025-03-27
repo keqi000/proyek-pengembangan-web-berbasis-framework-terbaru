ce97731a5edfe1bc5d7f856f0b6fd101ee2d73ff
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2025-03-27 16:22:30 +0800

    [page:admin] set redirect page in `/admin` to `/admin/home`

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,6 @@
+import { redirect } from "next/navigation";
+
+export function AdminTemporaryPage(){
+  // TODO: set checker in middleware and probably in this pages
+  redirect('/admin/login')  
+}
