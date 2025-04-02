49e240c252ce88643979257fc50dd92234a9f6ad
Yonathan Sihotang <143815443+sihotang-yonathan1@users.noreply.github.com>
2025-04-02 14:33:42 +0800

    [_query/course] Add `useCreateCourse`

--- src/app/admin/_query/course.ts
+++ src/app/admin/_query/course.ts
@@ -1,9 +1,27 @@
-import { useQuery } from "@tanstack/react-query"
-import { getAllCourse } from "../_service/course"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
+import { createCourse, getAllCourse } from "../_service/course"
+import { CourseItem } from "../_schema/course"
 
 export function useCourseInfo(){
   return useQuery({
     queryKey: ["course"],
     queryFn: () => getAllCourse()
+  })
+}
+
+type CreateCourseProps = Omit<CourseItem, "id">
+
+export function useCreateCourse(){
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationKey: ['course'],
+    mutationFn: (data: CreateCourseProps) => createCourse(data),
+    onSuccess: (data, variable, context) => {
+      console.log("Success to create course")
+      queryClient.invalidateQueries({queryKey: ['course']})
+    },
+    onError: (error, variable, context) => {
+      console.error(error.message)
+    }
   })
 }
