c6ee1ad740550d29bd2477a860994919f5ea8391
Yonathan Sihotang <143815443+sihotang-yonathan1@users.noreply.github.com>
2025-04-05 01:57:07 +0800

    [_query/course] Add `useUpdateCourse` function

--- src/app/admin/_query/course.ts
+++ src/app/admin/_query/course.ts
@@ -1,5 +1,5 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
-import { createCourse, getAllCourse } from "../_service/course"
+import { createCourse, getAllCourse, updateCourse } from "../_service/course"
 import { CourseItem } from "../_schema/course"
 
 export function useCourseInfo(){
@@ -18,6 +18,24 @@ export function useCreateCourse(){
     mutationFn: (data: CreateCourseProps) => createCourse(data),
     onSuccess: (data, variable, context) => {
       console.log("Success to create course")
+      queryClient.invalidateQueries({queryKey: ['course']})
+    },
+    onError: (error, variable, context) => {
+      console.error(error.message)
+    },
+    onSettled: (data, error, variable) => {
+      queryClient.invalidateQueries({queryKey: ['course']})
+    }
+  })
+}
+
+export function useUpdateCourse(){
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationKey: ['course'],
+    mutationFn: (updateData: CourseItem) => updateCourse(updateData),
+    onSuccess: (data, variable, context) => {
+      console.log("Success to update course")
       queryClient.invalidateQueries({queryKey: ['course']})
     },
     onError: (error, variable, context) => {
