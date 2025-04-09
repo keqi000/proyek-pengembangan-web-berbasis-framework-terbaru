174a4cafe8c1d030ecfb83b0e003de9453a50e12
Yonathan Sihotang <143815443+sihotang-yonathan1@users.noreply.github.com>
2025-04-10 03:20:51 +0800

    [admin/lecturer] Set search bar to be more responsive (not too big at mobile)

--- src/app/admin/lecturers/page.tsx
+++ src/app/admin/lecturers/page.tsx
@@ -82,20 +82,20 @@ const KelolaDosen = () => {
               Mengelola data dosen untuk sistem penjadwalan
             </p>
           </div>
-          <div className="flex space-x-3">
+          <div className="flex flex-col md:flex-row w-full md:w-auto space-x-3 gap-y-2">
             <div className="relative">
               <input
                 type="text"
                 placeholder="Cari dosen atau NIP..."
-                className="pl-10 pr-4 py-2 border border-white rounded-lg focus:outline-none ring-1 ring-gray-400 focus:ring-1 focus:ring-[#4F959D] w-full md:w-64 text-gray-600"
+                className="pl-8 pr-2 py-1.5 md:pl-10 md:pr-4 md:py-2 border border-white rounded-lg focus:outline-none ring-1 ring-gray-400 focus:ring-1 focus:ring-[#4F959D] w-full md:w-64 text-gray-600 text-sm md:text-base"
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
               />
-              <FaSearch className="absolute left-3 top-3 text-gray-400" />
+              <FaSearch className="absolute left-2.5 top-2.5 md:left-3 md:top-3 text-gray-400 text-xs md:text-base" />
             </div>
             <button
               onClick={() => setShowAddForm(!showAddForm)}
-              className="bg-[#4F959D] text-white px-4 py-2 rounded-lg hover:bg-[#3C7A85] transition flex items-center"
+              className="bg-[#4F959D] text-white px-3 py-1.5 md:px-4 md:py-2 text-xs md:text-sm rounded-lg hover:bg-[#3C7A85] transition flex items-center justify-center"
             >
               <FaPlus className="mr-2" /> Tambah Dosen
             </button>
