b99acf34f7ced4f7c312a333149eb2947657a911
Yonathan Sihotang <143815443+sihotang-yonathan1@users.noreply.github.com>
2025-04-10 04:12:51 +0800

    nit: change the breakpoint (from `sm` to `md` and `md` to `lg`)

--- src/app/admin/generate_schedule/page.tsx
+++ src/app/admin/generate_schedule/page.tsx
@@ -148,7 +148,7 @@ const GenerateJadwal = () => {
       <div className="bg-white p-2 sm:p-3 md:p-4 rounded-lg shadow-md mb-2 sm:mb-3">
         <div className="flex flex-col md:flex-row justify-between items-center gap-2">
           <div className="text-center md:text-left">
-            <h1 className="text-lg sm:text-xl md:text-2xl font-bold text-[#2C3930]">
+            <h1 className="text-lg md:text-xl lg:text-2xl font-bold text-[#2C3930]">
               Generate <span className="text-[#4F959D]">Jadwal</span>
             </h1>
             <p className="text-gray-600 mt-0.5 text-xs sm:text-sm">
