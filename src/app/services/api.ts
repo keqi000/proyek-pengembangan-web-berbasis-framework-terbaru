120a568f54e5c3570464bfae73c2fac51005790e
keqi000 <febrianhemeto04@gmail.com>
2025-05-11 21:37:42 +0800

    mengkonsikan backend dengan frontend pada halaman room

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -151,3 +151,50 @@ export const getMataKuliahForDosen = async (dosenId: string) => {
     throw error;
   }
 };
+
+// RUANGAN
+export const fetchRuangan = async () => {
+  try {
+    const response = await axios.get(`${API_URL}/rooms`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching rooms:", error);
+    throw error;
+  }
+};
+
+export const createRuangan = async (data: {
+  nama: string;
+  kapasitas: string;
+}) => {
+  try {
+    const response = await axios.post(`${API_URL}/rooms`, data);
+    return response.data;
+  } catch (error) {
+    console.error("Error creating room:", error);
+    throw error;
+  }
+};
+
+export const updateRuangan = async (
+  id: string,
+  data: { nama: string; kapasitas: string }
+) => {
+  try {
+    const response = await axios.patch(`${API_URL}/rooms/${id}`, data);
+    return response.data;
+  } catch (error) {
+    console.error("Error updating room:", error);
+    throw error;
+  }
+};
+
+export const deleteRuangan = async (id: string) => {
+  try {
+    const response = await axios.delete(`${API_URL}/rooms/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error deleting room:", error);
+    throw error;
+  }
+};
