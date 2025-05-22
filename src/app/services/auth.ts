090024566ef055e507a4b42affdb07c14c26aaec
Yonathan Sihotang <143815443+sihotang-yonathan1@users.noreply.github.com>
2025-05-22 12:40:55 +0800

    feat: Add `loginUSer` API call
    Not Tested Yet

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -5,8 +5,17 @@ type RegisterUserRequestDataType = {
   password: string;
 }
 
+type LoginUserRequestDataType = RegisterUserRequestDataType
+
+// TODO: include role
 export async function registerUser(data: RegisterUserRequestDataType){
   const response = axios.post(`${process.env.NEXT_PUBLIC_APi_BASE_URL}/register`, data)
 
+  return response
+}
+
+export async function loginUser(data: LoginUserRequestDataType){
+  const response = axios.post(`${process.env.NEXT_PUBLIC_APi_BASE_URL}/login`, data)
+  
   return response
 }
