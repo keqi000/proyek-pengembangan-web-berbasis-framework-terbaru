3c2ed0943767830c109394f2c11883dab4880d8e
sihotang-yonathan1 <143815443+sihotang-yonathan1@users.noreply.github.com>
2025-06-05 21:34:18 +0800

    fix: fix current username with actual data (#80)

---
+++ src/app/services/userInfo.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+type UserInfoData = {
+    id: number,
+    username: string,
+    role: string,
+    email_verified_at: null | string,
+    created_at: null | string,
+    updated_at: null | string
+        
+}
+
+type UserInfoResponse = {
+    message: string,
+    data: UserInfoData
+}
+
+export const fetchUserInfo = async () => {
+  try {
+    const response = await axios.get<UserInfoResponse>(`${process.env.NEXT_PUBLIC_APi_BASE_URL}/me`);
+    return response.data;
+  } catch (error) {
+    console.error("Error user info:", error);
+    throw error;
+  }
+};
